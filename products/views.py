8affc9c16b0fdcebc1f69d4b5e5b69a0dfe9ebe4
afratetlay <afratetlay@gmail.com>
2020-07-16 15:33:41 +0000

    working on my gallery

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """ A view to show all of the products, including the sorting and the search queries """
+
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
