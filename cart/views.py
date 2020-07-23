97d4d2a74558ef25fd90f3eb20efdea526f11b15
afratetlay <afratetlay@gmail.com>
2020-07-23 10:51:30 +0000

    working on my add to bag

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,12 @@ def view_cart(request):
     """ A view to the cart page """
 
     return render(request, 'cart/cart.html')
+
+def add_to_bag(request, item_id):
+    """ Add items to the bag """
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
