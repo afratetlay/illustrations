faa29d198d167c37ede04e0debe9f5b890064549
afratetlay <afratetlay@gmail.com>
2020-07-27 15:47:27 +0000

    i have finsihed off my remove button

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from products.models import Product
 # Create your views here.
 
 def view_cart(request):
@@ -9,7 +11,7 @@ def view_cart(request):
 
 def add_to_bag(request, item_id):
     """ Add items to the bag """
-
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -18,17 +20,54 @@ def add_to_bag(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
     
     request.session['cart'] = cart
     return redirect(redirect_url)
 
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    cart = request.session.get('cart', {})
+
+    if size:
+        if quantity > 0:
+            cart[item_id]['items_by_size'][size] = quantity
+        else:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
 def remove_from_bag(request, item_id):
-    """ Remove the item from the shopping bag """
+    """Remove the item from the shopping bag"""
+    print("Called")
     try:
-        if item_id:
-            bag.pop(item_id) 
-        
-        request.session['cart'] = cart 
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        cart = request.session.get('cart', {})
+
+        if size:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+        else:
+            cart.pop(item_id)
+
+        request.session['cart'] = cart
         return HttpResponse(status=200)
+
     except Exception as e:
         return HttpResponse(status=500)
