965b7a45d966c3cc8490f2c892626771badf1cbd
afratetlay <afratetlay@gmail.com>
2020-07-30 09:40:54 +0000

    created checkout app and models

--- checkout/models.py
+++ checkout/models.py
@@ -1,16 +1,8 @@
-import uuid
-
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
-from products.models import Product
-
-
-   """ Model to store order information, customer, 
-    date ordered and if the order has been paid yet,
-    I have not included address as the product will be sent 
-    to the customer via email. """
+from products.models import Product 
 
 class Order(models.Model):
 
@@ -21,7 +13,12 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def __str__(self):
+        return 'Congratulations {self.fullname}, please check your email for your illustrations'.format(self=self)
+
 class OrderLineItem(models.Model):
     
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return 'You have ordered {self.product}'.format(self=self)
