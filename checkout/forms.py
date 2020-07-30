10ef49df78f77977575e7e66d494cefd0cb64df2
afratetlay <afratetlay@gmail.com>
2020-07-30 15:50:00 +0000

    working on my checkout html page

--- checkout/forms.py
+++ checkout/forms.py
@@ -16,12 +16,12 @@ def __init__(self, *args, **kwargs):
         'phone_number': 'Phone Number',
     }
 
-self.fields['full_name'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
+    self.fields['full_name'].widget.attrs['autofocus'] = True
+    for field in self.fields:
+        if self.fields[field].required:
+            placeholder = f'{placeholders[field]} *'
+        else:
+            placeholder = placeholders[field]
+        self.fields[field].widget.attrs['placeholder'] = placeholder
+    self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+    self.fields[field].label = False
